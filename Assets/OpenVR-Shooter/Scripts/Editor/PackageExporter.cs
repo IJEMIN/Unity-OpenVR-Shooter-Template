5b773fa6b15f3a5fa3ab246ac8d2e2812d667a7d
I_Jemin <i_jemin@hotmail.com>
2017-12-03 19:14:56 +0900

    Add package exporter

---
+++ Assets/OpenVR-Shooter/Scripts/Editor/PackageExporter.cs
@@ -0,0 +1,19 @@
+﻿// 단순 유니티 패키지 제작용 스크립트
+
+
+using UnityEngine;
+using System.Collections;
+using UnityEditor;
+ 
+public static class ExportPackage {
+ 
+
+    [MenuItem("Export/Export with tags and layers, Input settings")]
+    public static void export()
+    {
+        string[] projectContent = new string[] {"Assets", "ProjectSettings/TagManager.asset","ProjectSettings/InputManager.asset","ProjectSettings/ProjectSettings.asset"};
+        AssetDatabase.ExportPackage(projectContent, "OpenVR-Shooter.unitypackage",ExportPackageOptions.Interactive | ExportPackageOptions.Recurse |ExportPackageOptions.IncludeDependencies);
+        Debug.Log("Project Exported");
+    }
+ 
+}
